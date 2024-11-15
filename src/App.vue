1f8c2750e35c34308779a8c934d5325924e57b58
Rehan-Paudel <rehanpoudel2@gmail.com>
2024-11-15 10:18:29 +0545

    first time added

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import LandPage from './components/LandPage.vue';
+
+
+
+</script>
+<template>
+  <LandPage/>
+</template>
---
+++ src/components/LandPage.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+const text = "Hello, World!";
+</script>
+
+<template>
+
+  <p> {{ text }} from vue js which iam currently learning</p>
+</template>
